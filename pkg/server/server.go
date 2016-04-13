b85d492eee88f97d721c9fb152d3e2532132efe3
Josh Bowen <jbowen@apigee.com>
2016-04-13 13:49:34 -0600

    WIP, CRUD of deployments is working.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -256,6 +256,8 @@ func createDeployment(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Printf("Error decoding PTS JSON Body: %v\n", err)
 	}
+	tempPTS.Annotations["trafficHosts"] = tempJSON.TrafficHosts
+	tempPTS.Annotations["trafficWeights"] = tempJSON.TrafficWeights
 
 	template := extensions.Deployment{
 		ObjectMeta: api.ObjectMeta{
@@ -278,7 +280,7 @@ func createDeployment(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Error creating deployment: %v\n", err)
 		return
 	}
-	fmt.Printf("Created Deployment: %v\n", dep.GetName())
+	fmt.Fprintf(w, "Created Deployment: %v\n", dep.GetName())
 }
 
 //getDeployment returns a deployment matching the given environmentGroupID, environmentName, and deploymentName
@@ -300,10 +302,42 @@ func getDeployment(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//TODO:
 //updateDeployment updates a deployment matching the given environmentGroupID, environmentName, and deploymentName
 func updateDeployment(w http.ResponseWriter, r *http.Request) {
-	// pathVars := mux.Vars(r)
+	pathVars := mux.Vars(r)
+
+	//Struct to put JSON into
+	type deploymentPatch struct {
+		TrafficHosts   string `json:"trafficHosts"`
+		TrafficWeights string `json:"trafficWeights"`
+		Replicas       int    `json:"Replicas"`
+	}
+	//Decode passed JSON body
+	decoder := json.NewDecoder(r.Body)
+	var tempJSON deploymentPatch
+	err := decoder.Decode(&tempJSON)
+	if err != nil {
+		fmt.Printf("Error decoding JSON Body: %v\n", err)
+		return
+	}
+
+	getDep, err := client.Deployments(pathVars["environmentGroupID"] + "-" + pathVars["environment"]).Get(pathVars["deployment"])
+	if err != nil {
+		fmt.Printf("Error getting old deployment: %v\n", err)
+	}
+	getDep.Spec.Replicas = tempJSON.Replicas
+
+	//Make map first?
+	getDep.Spec.Template.ObjectMeta.Annotations = make(map[string]string)
+	getDep.Spec.Template.Annotations["trafficHosts"] = tempJSON.TrafficHosts
+	getDep.Spec.Template.Annotations["trafficWeights"] = tempJSON.TrafficWeights
+
+	dep, err := client.Deployments(pathVars["environmentGroupID"] + "-" + pathVars["environment"]).Update(getDep)
+	if err != nil {
+		fmt.Printf("Error updating deployment: %v\n", err)
+		return
+	}
+	fmt.Fprintf(w, "Updated Deployment: %v\n", dep.GetName())
 
 }
 
@@ -317,158 +351,3 @@ func deleteDeployment(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Deleted Deployment: %v\n", pathVars["deployment"])
 }
-
-//TODO: Everything below this should go away
-
-/*
-//ApplicationHandler does stuff
-func ApplicationHandler(w http.ResponseWriter, r *http.Request) {
-
-	//get the variable path
-	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Path: /%s\n", vars["Namespace"])
-
-	//get the http verb
-	verb := r.Method
-	fmt.Fprintf(w, "HTTP Verb: %s\n", verb)
-
-	//get namespace matching vars["Namespace"]
-	imagedeployment := enrober.ImageDeployment{
-		Namespace:    vars["Namespace"],
-		Application:  vars["application"],
-		Revision:     "",
-		TrafficHosts: []string{},
-		PublicPaths:  []string{},
-		PathPort:     "",
-		PodCount:     0,
-	}
-
-	//Case statement based on http verb
-	switch verb {
-
-	case "GET":
-		depList, err := dm.GetDeploymentList(imagedeployment)
-		if err != nil {
-			fmt.Fprintf(w, "Broke at deployment: %v\n", err)
-			fmt.Fprintf(w, "In function ApplicationHandler\n")
-			return
-		}
-		for _, dep := range depList.Items {
-			fmt.Fprintf(w, "Got Deployment %v\n", dep.GetName())
-		}
-	}
-}
-
-//RevisionHandler does stuff
-func RevisionHandler(w http.ResponseWriter, r *http.Request) {
-	//get the variable path
-	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Path: /%s\n", vars["Namespace"])
-
-	//get the http verb
-	verb := r.Method
-	fmt.Fprintf(w, "HTTP Verb: %s\n", verb)
-
-	imagedeployment := enrober.ImageDeployment{
-		Namespace:    vars["Namespace"],
-		Application:  vars["application"],
-		Revision:     vars["revision"],
-		TrafficHosts: []string{},
-		PublicPaths:  []string{},
-		PathPort:     "",
-		PodCount:     1,
-		EnvVars:      map[string]string{},
-	}
-
-	//Case statement based on http verb
-	switch verb {
-
-	case "GET":
-		dep, err := dm.GetDeployment(imagedeployment)
-		if err != nil {
-			fmt.Fprintf(w, "Broke at deployment: %v\n", err)
-			fmt.Fprintf(w, "In function RevisionHandler\n")
-			return
-		}
-		fmt.Fprintf(w, "Got Deployment %v\n", dep.GetName())
-
-	case "PUT", "POST":
-		//Check if namespace already exists
-		getNs, err := dm.GetNamespace(imagedeployment)
-
-		//TODO: No longer creating namespace here
-		//Namespace wasn't found so create it
-		if err != nil && getNs.GetName() == "" {
-			ns, err := dm.CreateNamespace(imagedeployment)
-			if err != nil {
-				fmt.Fprintf(w, "Broke at namespace: %v\n", err)
-				fmt.Fprintf(w, "In function RevisionHandler\n")
-				return
-			}
-			fmt.Fprintf(w, "Put Namespace %s\n", ns.GetName())
-		}
-
-		//TODO: Possibly put types somewhere else
-		//Where I'm putting the JSON body
-		type deploymentVariables struct {
-			PodCount        int               `json:"podCount"`
-			Image           string            `json:"image"`
-			ImagePullSecret string            `json:"imagePullSecret"`
-			TrafficHosts    []string          `json:"trafficHosts"`
-			PublicPaths     []string          `json:"publicPaths"`
-			PathPort        int               `json:"pathPort"`
-			EnvVars         map[string]string `json:"envVars"`
-		}
-
-		//TODO: Probably a horrifying amount of input validation
-		decoder := json.NewDecoder(r.Body)
-		var t deploymentVariables
-		err = decoder.Decode(&t)
-		if err != nil {
-			fmt.Printf("Error decoding JSON")
-		}
-
-		//TrafficHosts can't be empty so fail if it is
-		//TODO: Should do this checking before we create the namespace
-		if t.TrafficHosts[0] == "" {
-			fmt.Fprintf(w, "Traffic Hosts cannot be empty")
-			return
-		}
-
-		//TODO: This feels repetitive
-		imagedeployment.PodCount = t.PodCount
-		imagedeployment.Image = t.Image
-		imagedeployment.ImagePullSecret = t.ImagePullSecret
-		imagedeployment.TrafficHosts = t.TrafficHosts
-		imagedeployment.PublicPaths = t.PublicPaths
-		imagedeployment.PathPort = strconv.Itoa(t.PathPort)
-		imagedeployment.EnvVars = t.EnvVars
-
-		//Check if deployment already exists
-		getDep, err := dm.GetDeployment(imagedeployment)
-
-		//Deployment wasn't found so create it
-		if err != nil && getDep.GetName() == "" {
-
-			//TODO: Should create a secret here?
-
-			dep, err := dm.CreateDeployment(imagedeployment)
-			if err != nil {
-				fmt.Fprintf(w, "Broke at deployment: %v\n", err)
-				fmt.Fprintf(w, "In function RevisionHandler\n")
-				return
-			}
-			fmt.Fprintf(w, "New Deployment %s\n", dep.GetName())
-		} else {
-			//Deployment was found so modify it
-			dep, err := dm.UpdateDeployment(imagedeployment)
-			if err != nil {
-				fmt.Fprintf(w, "Broke at deployment: %v\n", err)
-				fmt.Fprintf(w, "In function RevisionHandler\n")
-				return
-			}
-			fmt.Fprintf(w, "Modified Deployment %s\n", dep.GetName())
-		}
-	}
-}
-*/
