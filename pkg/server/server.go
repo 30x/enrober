d7603d5d2fdbecf46fe893e1fa7ab91699c50cda
Josh Bowen <jbowen@apigee.com>
2016-08-17 16:36:29 -0600

    Added support to redirect to private router based on env variables.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -52,6 +53,10 @@ var (
 
 	//Namespace Isolation
 	isolateNamespace bool
+
+	shipyardHost          string
+	internalRouterHost    string
+	shipyardPrivateSecret string
 )
 
 //Init runs once
@@ -76,6 +81,10 @@ func Init(clientConfig restclient.Config) error {
 		}
 		client = *tempClient
 	}
+
+	shipyardHost = os.Getenv("SHIPYARD_HOST")
+	internalRouterHost = os.Getenv("INTERNAL_ROUTER_HOST")
+	shipyardPrivateSecret = os.Getenv("SHIPYARD_PRIVATE_SECRET")
 
 	if os.Getenv("ISOLATE_NAMESPACE") == "false" {
 		isolateNamespace = false
@@ -662,17 +671,35 @@ func createDeployment(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		//Get from URL
-		//TODO: Duplicated code, could be moved to helper function
-		//Get JSON from url
 
 		httpClient := &http.Client{}
 
-		req, err := http.NewRequest("GET", tempJSON.PtsURL, nil)
+		ptsURL, err := url.Parse(tempJSON.PtsURL)
+		if err != nil {
+			errorMessage := fmt.Sprintf("Error parsing ptsURL\n")
+			http.Error(w, errorMessage, http.StatusInternalServerError)
+			helper.LogError.Printf(errorMessage)
+			return
+		}
+
+		internalRouterFlag := false
+
+		if ptsURL.Host == shipyardHost {
+			ptsURL.Host = internalRouterHost
+			ptsURL.Scheme = "http"
+			internalRouterFlag = true
+		}
+
+		req, err := http.NewRequest("GET", ptsURL.String(), nil)
+
+		if internalRouterFlag {
+			req.Host = shipyardHost
+			req.Header.Add("Host", shipyardHost)
+			req.Header.Add("X-ROUTING-API-KEY", base64.StdEncoding.EncodeToString([]byte(shipyardPrivateSecret)))
+		}
 		req.Header.Add("Authorization", r.Header.Get("Authorization"))
 		req.Header.Add("Content-Type", "application/json")
 
-		//TODO: In the future if we require a secret to access the PTS store
-		// then this call will need to pass in that key.
 		urlJSON, err := httpClient.Do(req)
 		if err != nil {
 			errorMessage := fmt.Sprintf("Error retrieving pod template spec: %s\n", err)
@@ -911,12 +938,32 @@ func updateDeployment(w http.ResponseWriter, r *http.Request) {
 			//Get JSON from url
 			httpClient := &http.Client{}
 
-			req, err := http.NewRequest("GET", tempJSON.PtsURL, nil)
+			ptsURL, err := url.Parse(tempJSON.PtsURL)
+			if err != nil {
+				errorMessage := fmt.Sprintf("Error parsing ptsURL\n")
+				http.Error(w, errorMessage, http.StatusInternalServerError)
+				helper.LogError.Printf(errorMessage)
+				return
+			}
+
+			internalRouterFlag := false
+
+			if ptsURL.Host == shipyardHost {
+				ptsURL.Host = internalRouterHost
+				ptsURL.Scheme = "http"
+				internalRouterFlag = true
+			}
+
+			req, err := http.NewRequest("GET", ptsURL.String(), nil)
+
+			if internalRouterFlag {
+				req.Host = shipyardHost
+				req.Header.Add("Host", shipyardHost)
+				req.Header.Add("X-ROUTING-API-KEY", base64.StdEncoding.EncodeToString([]byte(shipyardPrivateSecret)))
+			}
 			req.Header.Add("Authorization", r.Header.Get("Authorization"))
 			req.Header.Add("Content-Type", "application/json")
 
-			//TODO: In the future if we require a secret to access the PTS store
-			// then this call will need to pass in that key.
 			urlJSON, err := httpClient.Do(req)
 			if err != nil {
 				errorMessage := fmt.Sprintf("Error retrieving pod template spec: %s\n", err)
